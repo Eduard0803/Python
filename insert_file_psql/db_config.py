352f9f9ed29009148dd71c0bbaff9e8858ff99ed
Eduard0803 <eduardo83204gmail.com>
2023-08-03 14:28:52 -0300

    insert new script 'insert_file_psql'

---
+++ insert_file_psql/db_config.py
@@ -0,0 +1,15 @@
+from sqlalchemy import create_engine, Column, Integer, LargeBinary, String, MetaData
+from sqlalchemy.orm import declarative_base, sessionmaker
+
+DATABASE_URL = 'postgresql://username:password@host_adreess:port_number/db_name'
+engine = create_engine(DATABASE_URL)
+Base = declarative_base()
+Session = sessionmaker(bind=engine)
+
+class DefaultTable(Base):
+    __tablename__ = 'files'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    file_name = Column(String(100))
+    file_content = Column(LargeBinary)
+
+Base.metadata.create_all(bind=engine)
---
+++ insert_file_psql/main.py
@@ -0,0 +1,41 @@
+import os
+from db_config import Session, DefaultTable
+
+def insert_file(file_name, file_content):
+    session = Session()
+    new_file = DefaultTable(file_name=file_name, file_content=file_content)
+    session.add(new_file)
+    session.commit()
+    session.close()
+
+def query_file_by_name(file_name):
+    session = Session()
+    file = session.query(DefaultTable).filter_by(file_name=file_name).first()
+    if file is None:
+        return None
+    return file
+
+
+if __name__ == "__main__":
+
+    file_path = input('Insert the file name: ')
+
+    if os.path.exists(file_path):
+
+        option = int(input('\n1 - insert file\n2 - query file\n\nselect the option: '))
+        if option == 1:
+            with open(file_path, 'rb') as file:
+                file_data = file.read()
+            insert_file(file_path, file_data)
+            print('sucess')
+        else:
+            content = query_file_by_name(file_path)
+            if not content:
+                print('file not found')
+            else:
+                name = input('input the new name: ')
+                with open(name, 'wb') as file:
+                    file.write(content.file_content)
+                print('sucess')
+    else:
+        print('file not found')
