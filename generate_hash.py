8b3f2483107b5b67650145d3b503181485f1cbb8
Eduard0803 <eduardo83204gmail.com>
2023-09-16 10:26:30 -0300

    add 'generate_hash.py'

---
+++ generate_hash.py
@@ -0,0 +1,19 @@
+import hashlib, os
+
+def str_hash(string:str=None)->None:
+    if not string:
+        print('string not found')
+        return
+    md5_hash = hashlib.md5(string.encode()).hexdigest()
+    print(f'string_hash: {md5_hash}')
+
+def file_hash(file_name:str=None)->None:
+    if not file_name or not os.path.exists(file_name):
+        print('file not found')
+        return
+    md5_hash = hashlib.md5(open(file_name, "rb").read()).hexdigest()
+    print(f'hash_generated: {md5_hash}')
+
+if __name__ == '__main__':
+    str_hash('example')
+    file_hash('example.pdf')
