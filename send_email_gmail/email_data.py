330d9f6c4c4f53e8ad2bd863422c0284f7c3d526
Eduard0803 <eduardo83204gmail.com>
2023-09-18 19:08:05 -0300

    insert directory 'send_email_gmail'

---
+++ send_email_gmail/email_data.py
@@ -0,0 +1,10 @@
+email_data = {
+    'your_email': 'YOUR EMAIL',
+    'email_password': 'YOUR PASSWORD',
+    'email_subject': 'SUBJECT',
+    'email_body': '''
+                    <p>Teste de Funcionamento<p>
+                    <p>Nome: {}<p>
+                    <p>Mensagem: {}<p>
+                '''
+}
